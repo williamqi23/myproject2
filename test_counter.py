67dd1195df0428dcedd069a8c4f8d16f46f8214e
WilliamQi <at23@139.com>
2022-03-27 10:44:56 +0800

    testcounter

--- test_counter.py
+++ test_counter.py
@@ -3,3 +3,11 @@ from unittest import TestCase
 
 class TestCounter(TestCase):
     pass
+
+
+class TestCounter(TestCase):
+    def test_update(self):
+        self.fail()
+
+    def test_run(self):
+        self.fail()
